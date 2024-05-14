2c6cb98f7b210f8cffab201cc780cd425bd09506
Your Name <you@example.com>
2024-05-14 17:45:05 +0400

    Language

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory,HasTranslations;
 
     protected $translatable=['name','slug'];
 
